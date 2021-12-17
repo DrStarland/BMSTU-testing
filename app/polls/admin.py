7ecfdb888dd4ef7ceb0c2b560678868918ae2e42
DrStarland <66081492+DrStarland@users.noreply.github.com>
2021-12-18 02:35:46 +0300

    Delete app directory

--- app/polls/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import ProductType, Product, Rating, Comment, Transaction, Cart
-
-admin.site.register(ProductType)
-admin.site.register(Product)
-admin.site.register(Rating)
-admin.site.register(Comment)
-admin.site.register(Transaction)
-admin.site.register(Cart)
--- app/webmarket/settings_read_only.py
+++
@@ -1,5 +0,0 @@
-from .settings import *
-
-INSTALLED_APPS.append('django_read_only')
-
-DJANGO_READ_ONLY = True
